5a8c32bbc9f31265964052281c758e2bd474b100
qingming <qingming1@staff.sina.com.cn>
2014-10-31 12:13:00 +0800

    support config.

--- js/wsyc.js
+++ js/wsyc.js
@@ -1,31 +1,28 @@
 
-var start = "17";
-var end = "19";
-var day = "2014-11-03";
-var trainsessionid="04";
-
 var wsyc = {
     totalCount_: 0,
     url_: 'http://111.204.39.58:8080/Ajax/StuHdl.ashx?' +
         'loginType=2&' +
         'method=yueche&' + 
-        'start=' + start + '&' +
-        'end=' + end + '&' +
-        'lessionid=001&' +
         'lesstypeid=02&' +
-        'date=' + day + '&' +
         'id=1&' +
         'carid=&' + 
         'ycmethod=03&' +
         'cartypeid=02&' +
-        'trainsessionid=' + trainsessionid + '&' +
         'ReleaseCarID=',
 
     btnWsycClick: function() {
         var stuid = $("#fchrStudentID").val();
         var bmnum = $("#fchrRegistrationID").val();
         var trainpriceid = $("#fchrTrainPriceID").val();
+        var day = $("#date").val();
+        var start = $("#start").val();
+        var end = $("#end").val();
+        var trainsessionid = $("#trainsessionid").val();
+        var lessionid = $("#lessionid").val();
         var url = this.url_ + '&stuid=' + stuid + '&bmnum=' + bmnum + '&trainpriceid=' + trainpriceid;
+        url = url + '&start=' + start + '&end=' + end + '&date=' + day;
+        url = url + '&trainsessionid=' + trainsessionid + '&lessionid=' + lessionid;
         $.ajax({
             type: "post",
             cache: false,
@@ -37,7 +34,10 @@ var wsyc = {
 
     btnWsycClickSucc_: function(data) { 
         wsyc.totalCount_++;
-        console.log(day + " " + start + " " + end + " " + wsyc.totalCount_ + ": " + data);
+        var log = wsyc.totalCount_ + " response: " + data;
+        console.log(log);
+        var divLog = $("#divLog");
+        divLog.html(divLog.html() + log + "<br />");
         if(data == "success") {
             alert("success");
         } else if(data != "Timeout") {
@@ -52,14 +52,17 @@ var wsyc = {
     }
 };
 
+var html = '<table> <tr> <td>日期:</td> <td><input type="text" id="date" value="2014-11-03" /></td> <td>格式: 2014-01-01 </td> </tr> <tr> <td>开始时段:</td> <td><input type="text" id="start" value="17" /></td> <td>格式: 17 </td> </tr> <tr> <td>结束时段:</td> <td><input type="text" id="end" value="19" /></td> <td>格式: 19 </td> </tr> <tr> <td>时段编号:</td> <td><input type="text" id="trainsessionid" value="04" /></td> <td>01(7-9) 02(9-13) 03(13-17) 04(17-19) 05(19-21) </td> </tr> <tr> <td>训练阶段编号:</td> <td><input type="text" id="lessionid" value="001" /></td> <td>001(散段) 002(桩训) 003(法培) 006(模拟机) 007(综合训练) 008(考前培训) </td> </tr> </table>'
+
 var divWsyc = $(".ZF_xy_main");
-//$('<input type="button" class="submit" value="约车" onclick="" />').appendTo(divWsyc);
+$(html).appendTo(divWsyc);
 var btnWsyc = document.createElement("input");
 btnWsyc.id = "btnWsyc";
 btnWsyc.type = "button";
 btnWsyc.setAttribute("class", "submit");
 btnWsyc.value = "约车";
 divWsyc.append(btnWsyc);
+$('<div id="divLog" style="width:500; height:500;"></div>').appendTo(divWsyc);
 
 btnWsyc.onclick = function() {
     wsyc.btnWsycClick();    
